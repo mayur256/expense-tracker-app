b3e39375e94427be5c85424011a9d4e83eeec622
Mayur Upadhayay <mayur123a@gmail.com>
2024-04-23 23:19:55 +0530

    chore: Install Antdesign template

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import "./assets/main.css";
+import 'ant-design-vue/dist/reset.css';
+import './scss/app.scss';
 
 import { createApp } from "vue";
+import Antd from 'ant-design-vue';
 import App from "./App.vue";
 // import router from "./router";
 
 const app = createApp(App);
 
+app.use(Antd);
 // app.use(router);
 
 app.mount("#app");
