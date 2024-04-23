79c115e2573f06126acfd94a6ca3772edf5a68d4
Mayur Upadhayay <mayur123a@gmail.com>
2024-04-24 00:31:37 +0530

    chore: Tweak typescript support for vue

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import './scss/app.scss';
 import { createApp } from "vue";
 import Antd from 'ant-design-vue';
 import App from "./App.vue";
+
 // import router from "./router";
 
 const app = createApp(App);
